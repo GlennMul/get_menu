4485880245367690e41f56655b20a3a0f333690e
GlennMul <glennmulhall@gmail.com>
2020-05-07 14:27:59 +0800

    Try

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
-"""
+""""""
 df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
                    'B': [5, 6, 7, 8, 9],
                    'C': ['a', 'b', 'c--', 'd', 'e']})
-"""
+""""""
 
 # df = get_menu("https://www.ubereats.com/au/melbourne/food-delivery/ez-thai-cafe/osnrZYXYTbe6HFMa_JU-7g/27ced650-2a4b-4371-b22c-2bf9335b771a")
 
@@ -18,6 +18,6 @@ df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
 def test():
     args = request.args
     url = args.get("url")
-    df = get_menu(url)
+    #df = get_menu(url)
 
     return render_template("index.html", url=url, tables=[df.to_html(classes='u-full-width', index=False)])
