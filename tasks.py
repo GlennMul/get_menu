8940fcd88bffd8b709ea1c1853199c0f507388db
GlennMul <glennmulhall@gmail.com>
2020-05-08 10:36:33 +0800

    redis

--- tasks.py
+++ tasks.py
@@ -8,7 +8,5 @@ def add(x, y):
     return (x + y) * x
 
 
-import os
-
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+app.conf.update(BROKER_URL='redis://',
+                CELERY_RESULT_BACKEND='redis://')
