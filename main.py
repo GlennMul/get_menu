876f57e70dab30564400361a154c0534e8f877a0
GlennMul <glennmulhall@gmail.com>
2020-05-15 12:13:07 +0800

    url update

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def test():
+def index():
     args = request.args
     url = args.get("url")
     if url is not None:
         df = get_menu(url)
+
     else:
         df = pd.DataFrame([])
 
     return render_template("index.html", url=url, tables=[df.to_html(classes='u-full-width', index=False)])
 
 
-@app.route('/csv/')
-def download_csv():
-    url = "https://www.ubereats.com/au/melbourne/food-delivery/leonards-house-of-love/wB5LCl7BSNugoFXljJbKOQ"
+@app.route('/csv/<path:url>', methods=['GET', 'POST'])
+def download_csv(url):
     df = get_menu(url)
     resp = make_response(df.to_csv(index=False))
     resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
